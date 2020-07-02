28a22f00c4642a577ab9b9100fce704d0f918aaf
Niall Larson <nialldizzle@gmail.com>
2020-07-01 17:30:59 -0700

    database

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO burgers (burger_name) VALUES ("Beets of Burden Burger");
+INSERT INTO burgers (burger_name) VALUES ("Let's Give 'Em Something Shiitake 'Bout Burger");
+INSERT INTO burgers (burger_name) VALUES ("Sgt. Poblano Pepper Lonely Artichoke Hearts Club Burger");
+INSERT INTO burgers (burger_name) VALUES ("Papaya Was a Rolling Stone Burger");
+INSERT INTO burgers (burger_name) VALUES ("I Fought the Slaw Burger (And the Slaw Won)");
+INSERT INTO burgers (burger_name) VALUES ("Parma Parma Parma Chameleon Burger");
+INSERT INTO burgers (burger_name) VALUES ("The Final Kraut Down Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bohemian Radishy Burger");
+INSERT INTO burgers (burger_name) VALUES ("Don't Go Brocking My Heart Burger");
+INSERT INTO burgers (burger_name) VALUES ("Sweet Chili O'Mine Burger");
+INSERT INTO burgers (burger_name) VALUES ("Hit Me With Your Best Shallot Burger");
+INSERT INTO burgers (burger_name) VALUES ("Shake Your Honeymaker Burger");
+INSERT INTO burgers (burger_name) VALUES ("Sympathy for the Deviled Egg Burger");
+INSERT INTO burgers (burger_name) VALUES ("She's a Super Leek Burger");
+INSERT INTO burgers (burger_name) VALUES ("Sweet Home Avocado Burger");
